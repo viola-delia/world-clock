915d3b9fd8b17043fbd90bf63cd32a515b3dcfbc
viola-delia <viola.delia28@gmail.com>
2024-01-14 13:34:58 +1000

    Added current location option

--- javascript/index.js
+++ javascript/index.js
@@ -27,6 +27,10 @@ romeTimeElement.innerHTML = `${romeTime.format("H:mm:ss [<small>]A[</small>]")}`
 //Change the city in the container
 function updateCity(event) {
     let cityTimeZone = event.target.value;
+if (cityTimeZone === "current") {
+  cityTimeZone = moment.tz.guess();
+}
+
     let cityName = cityTimeZone.replace("_", " ").split("/")[1];
     let cityTime = moment().tz(cityTimeZone);
     let citiesElement = document.querySelector("#cities");
